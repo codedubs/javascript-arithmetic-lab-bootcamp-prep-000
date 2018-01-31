285cc459ea4639efd3d5161f54d0abba3ffcf819
Pitta <mailez4all@gmail.com>
2018-01-31 09:14:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-
-
 function add(a, b) {
   return a + b
 }
 
-add(2,3)
+add(2, 3)
 
-function subtract(a,b) {
-  return a-b
+function subtract(a, b) {
+  return a - b
 }
-subtract(5,4)
 
-function multiply() {
+subtract(5, 4)
 
+function multiply(a, b) {
+  return a * b
 }
-function divide() {
 
+multiply(4, 5)
+
+function divide(a, b) {
+  return a / b
 }
+
+divide(10, 2)
